62a176a883cae18830aece7efbef6676f1e1a24e
Jonathan Chapman <jrchaps@gmail.com>
2024-02-04 23:56:39 -0700

    Move slice to basic

--- basic/basic.h
+++ basic/basic.h
@@ -23,5 +23,30 @@ typedef double f64;
 #define true 1
 #define false 0 
 
-u32 ror(u32, u32);
-u32 be_to_u32(u8*);
+#define array_length(array) (sizeof(array) / sizeof(array[0]))
+
+#ifdef slice_bounds_checking
+    #ifndef assert
+        #error "You must define assert if using slices with bounds checking."
+    #endif
+#endif
+
+typedef struct slice slice;
+
+struct slice {
+    #ifdef slice_bounds_checking
+        size_t capacity;
+    #endif
+    size_t length;
+    u8* items;
+};
+
+#ifdef slice_bounds_checking
+    #define slice_make(capacity, length, buffer) { capacity, length, buffer }
+#else
+    #define slice_make(capacity, length, buffer) { length, buffer }
+#endif
+
+#define slice_alloc(capacity) slice_make(capacity, 0, (u8[capacity]) { 0 })
+#define slice_literal(...) slice_make(sizeof((u8[]) { __VA_ARGS__ }), sizeof((u8[]) { __VA_ARGS__ }), ((u8[]) { __VA_ARGS__ }))
+#define array_to_slice(array) (slice) slice_make(array_length(array), array_length(array), array)
